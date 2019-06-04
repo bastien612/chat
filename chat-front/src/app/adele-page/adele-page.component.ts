7a20f5bb064fb31877c0cc61d911630bac912ba8
Netsab612 <bastien.meunier@yahoo.fr>
2019-06-04 20:46:10 +0200

    Fix some stuff

--- chat-front/src/app/adele-page/adele-page.component.ts
+++ chat-front/src/app/adele-page/adele-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService }  from '../services/product.service';
+import { ProductService } from '../services/product.service';
 import { Product } from '../product';
+import { CodegenComponentFactoryResolver } from '@angular/core/src/linker/component_factory_resolver';
 
 @Component({
   selector: 'app-adele-page',
@@ -8,15 +9,17 @@ import { Product } from '../product';
   styleUrls: ['./adele-page.component.scss']
 })
 export class AdelePageComponent implements OnInit {
+  newProduct: Product = { id: 10, name: "Thon" };
 
-  constructor() { }
+
+  constructor(private productService: ProductService) { }
+
 
   ngOnInit() {
   }
 
   addProduct(): void {
-  //add en dur product
-    this.productService.postProduct(newProduct)
-    .subscribe(/*log consol objet envoye*/);
+    //add en dur product
+    this.productService.postProduct(this.newProduct).subscribe(response => console.log("Produit créé : ", response));
   }
 }
